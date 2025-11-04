da65fa3e61f4ccf8e928e7712ebf41439b5dae07
AshleyRFJ2 <AshleyRFJ2@gmail.com>
2025-11-03 23:21:58 -0600

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import weatherForecasts from "./components/WeatherForecast/WeatherForecast";
-
+import { Sun } from "lucide-react";
 import styles from "./components/WeatherForecast/WeatherForecast.module.css";
 
 const App = () => {
   return (
+  
     <section className={styles.weather}>
-      {weatherForecasts.map(({ day, img, imgAlt, conditions, time }) => (
+
+     {weatherForecasts.map(({ day, img, imgAlt, conditions, time }) => (
         <div className={styles.weather} key={day}>
           <h2>{day}</h2>
           <img src={img} alt={imgAlt} />
